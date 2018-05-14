a740f52362ff9bc497ad17347ae99e43eeb7ca2c
Akshay Jha <akshayjha@live.in>
2018-05-14 19:59:41 +0530

    Fixed the code

--- Exp 3/majority.v
+++ Exp 3/majority.v
@@ -1,18 +1,20 @@
 /*
 *	Majority circuit
+* 	Module returns true when more than half the inputs are true*
 *	Akshay - 24/02/2017
+* 	Bug fixed - 14/05/2018 . Thanks to Sumon.
 */
 
 /*
 module majority(a,b,c,d);
 	input a,b,c;
 	output d;
-	assign d = (a|b)&(b|c);
+	assign d = (a&b)|(b&c)|(c&a);
 endmodule
-]*/
+*/
 
 module majority(A, Z);
 	input [2:0]A;
 	output Z;
-	assign Z= (A[2]|A[1])&(A[1]|A[0]);
-	endmodule
+	assign Z= (A[2] & A[1]) | (A[1] & A[0]) | (A[0] & A[2]);
+endmodule
