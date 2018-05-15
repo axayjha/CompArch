a9737bb7a0a8a335b552a9e98df1d23e178952a5
Sumon Nath <34777377+thenath1998@users.noreply.github.com>
2018-05-15 20:07:57 +0530

    Update siso.v

--- Exp 10/siso.v
+++ Exp 10/siso.v
@@ -1,10 +1,23 @@
-module siso(clk,rst,a,q);
-	input a;
-	input clk,rst;
-	output q;
-	reg q;
-	always@(posedge clk,posedge rst)
-	begin
-		assign q=(rst==1)?0:a;
+module siso(
+    input in,
+    output out,
+    input rst,
+    input clk
+    );
+ reg [3:0] d;
+ reg out;
+ initial begin
+	d = 4'b0000;
+ end
+ always@(posedge clk) begin
+	if(rst == 1'b1) begin
+		d = 4'b0000;
+		out = 0;
 	end
+	else begin
+		out = d[0];
+		d = d >> 1;
+		d[3] = in;
+	end
+ end
 endmodule
